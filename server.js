b836eb0dd49aba8842f1e54ef1840d990056441d
ggjskjd <layan12rz@gmail.com>
2025-07-14 01:20:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.post('/api/login', (req, res) =>{
 
 });
 
-const PORT = Process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server running on port 3000'));
