c96d69ecabedd4f8d22b75bde10a28af45d5d0a0
ggjskjd <layan12rz@gmail.com>
2025-07-14 01:35:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'project')));
+app.use(express.static(path.join(__dirname, 'project')));  // <-- changed from 'public' to 'project'
 
 app.post('/api/login', (req, res) => {
-    console.log('Data received:', req.body);
-    res.status(200).send('OK');
+  console.log('Data received:', req.body);
+  res.status(200).send('OK');
 });
 
 const PORT = process.env.PORT || 3000;
